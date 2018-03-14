17ba58f1e2bfc898f83433fc5f1c3d124412d178
viktorsieger <23069803+viktorsieger@users.noreply.github.com>
2018-03-14 19:33:21 +0100

    Created (empty) activities and modified the manifest appropriately.

---
+++ app/src/main/java/se/umu/visi0009/comiccollector/activities/CollectionActivity.java
@@ -0,0 +1,12 @@
+package se.umu.visi0009.comiccollector.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CollectionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/se/umu/visi0009/comiccollector/activities/StatsActivity.java
@@ -0,0 +1,12 @@
+package se.umu.visi0009.comiccollector.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StatsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
