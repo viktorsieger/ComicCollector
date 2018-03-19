2abd3fca2076fd2e70e30e9ef9b0d600f3800328
Viktor Sieger <23069803+viktorsieger@users.noreply.github.com>
2018-03-19 19:22:39 +0100

    Multiple changes
    *Added Google Maps dependency to Gradle.
    *Added app permission for fine location access.
    *Replaced raster icon images with vector graphics.
    *Added hamburger menu to action bar.
    *Started adding Google Maps.

--- app/src/main/java/se/umu/visi0009/comiccollector/activities/MainActivity.java
+++ app/src/main/java/se/umu/visi0009/comiccollector/activities/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -31,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_hamburger);
+        }
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
@@ -85,4 +94,16 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
+    }
+
 }
