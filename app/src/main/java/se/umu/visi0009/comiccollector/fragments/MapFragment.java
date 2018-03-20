d591b356bb56243dbbc90ab2e08ab5c93ea3ae85
Viktor Sieger <23069803+viktorsieger@users.noreply.github.com>
2018-03-20 17:28:24 +0100

    Small fixes

--- app/src/main/java/se/umu/visi0009/comiccollector/fragments/MapFragment.java
+++ app/src/main/java/se/umu/visi0009/comiccollector/fragments/MapFragment.java
@@ -6,12 +6,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import se.umu.visi0009.comiccollector.R;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false); // OBS! Ändra layout!
+        View rootView =  inflater.inflate(R.layout.fragment_map, container, false);
+
+        ((SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
